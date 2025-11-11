dff96f904cbf390eeebb36cfd730669bfe986831
Luca Heger <l.heger@bluewin.ch>
2025-11-12 00:55:24 +0100

    Added the PlayerClass and the IGame.

---
+++ APROG_Raspi_Game/The_Game/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Game {
+    public class Player {
+        public Player(string Name, int Points) {
+            this.Name = Name;
+            this.Points = Points;
+            
+        }
+        public string Name { get; private set; }
+        public int Points { get; set; }
+    }
+}
